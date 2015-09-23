90550f6b8f79a694e2bc5a71b28f31d84bc0f9a5
Karol Wozniak <attu.git@email.com>
2015-09-23 20:56:52 +0200

    alias, rebind

---
+++ inc/archie/alias.hpp
@@ -0,0 +1,111 @@
+#pragma once
+#include <memory>
+
+namespace archie {
+template <typename T>
+struct alias_t {
+  using value_type = T;
+  using type = value_type;
+  using pointer = value_type*;
+  using const_pointer = value_type const*;
+  using reference = value_type&;
+  using const_reference = value_type const&;
+
+  struct rebind_t {
+    rebind_t(reference x) : data_(std::addressof(x)) {}
+    rebind_t(rebind_t const& x) = default;
+    pointer get() { return data_; }
+    const_pointer get() const { return data_; }
+
+  private:
+    pointer data_;
+  };
+
+  alias_t() = delete;
+  explicit alias_t(rebind_t r) : ref_(r) {}
+  explicit alias_t(reference indata) noexcept : ref_(indata) {}
+
+  alias_t(value_type&&) = delete;
+  alias_t(alias_t const&) = default;
+  alias_t(alias_t&&) = default;
+  template <typename U>
+  alias_t(alias_t<U> const& a)
+      : ref_(*a.get()) {}
+
+  alias_t& operator=(alias_t const&) = delete;
+  alias_t& operator=(alias_t&&) = default;
+
+  alias_t& operator=(rebind_t r) {
+    ref_ = r;
+    return *this;
+  }
+  alias_t& operator=(const_reference x) {
+    if (ref_.get() != &x) { *ref_.get() = x; }
+    return *this;
+  }
+  alias_t& operator=(value_type&& x) {
+    if (ref_.get() != &x) { *ref_.get() = std::move(x); }
+    return *this;
+  }
+
+  reference operator*() { return *ref_.get(); }
+  const_reference operator*() const { return *ref_.get(); }
+
+  pointer get() { return ref_.get(); }
+  const_pointer get() const { return ref_.get(); }
+
+  pointer operator->() { return get(); }
+  const_pointer operator->() const { return get(); }
+
+private:
+  rebind_t ref_;
+};
+
+template <typename T>
+bool operator==(alias_t<T> const& lhs, alias_t<T> const& rhs) {
+  return lhs.get() == rhs.get();
+}
+
+template <typename T>
+bool operator!=(alias_t<T> const& lhs, alias_t<T> const& rhs) {
+  return !(lhs == rhs);
+}
+
+template <typename T>
+bool operator<(alias_t<T> const& lhs, alias_t<T> const& rhs) {
+  return lhs.get() < rhs.get();
+}
+
+template <typename T>
+bool operator>(alias_t<T> const& lhs, alias_t<T> const& rhs) {
+  return rhs < lhs;
+}
+
+template <typename T>
+bool operator<=(alias_t<T> const& lhs, alias_t<T> const& rhs) {
+  return !(lhs > rhs);
+}
+
+template <typename T>
+bool operator>=(alias_t<T> const& lhs, alias_t<T> const& rhs) {
+  return !(lhs < rhs);
+}
+
+template <typename T>
+using rebind_t = typename alias_t<T>::rebind_t;
+
+template <typename T>
+constexpr alias_t<T> alias(T& t) noexcept {
+  return alias_t<T>(t);
+}
+
+template <typename T>
+constexpr rebind_t<T> rebind(T& t) noexcept {
+  return rebind_t<T>(t);
+}
+
+template <typename T>
+constexpr T& unwrap(alias_t<T> a) noexcept {
+  return *a;
+}
+}
---
+++ test/alias_test.cpp
@@ -0,0 +1,53 @@
+#include <archie/alias.hpp>
+#include <vector>
+#include <array>
+#include <catch.hpp>
+namespace {
+using namespace archie;
+
+TEST_CASE("alias", "[alias]") {
+  SECTION("ref") {
+    int x = 0;
+    int y = 1;
+    auto a = alias(x);
+    REQUIRE(*a == 0);
+    a = 2;
+    REQUIRE(x == 2);
+    a = rebind(y);
+    a = 3;
+    REQUIRE(x == 2);
+    REQUIRE(y == 3);
+  }
+  SECTION("const ref") {
+    const int x = 0;
+    const int y = 1;
+    auto a = alias(x);
+    REQUIRE(*a == 0);
+    a = rebind(y);
+    REQUIRE(*a == 1);
+  }
+  SECTION("sorting") {
+    std::array<int, 3> array = {3, 1, 2};
+    std::vector<alias_t<int>> vec;
+    vec.emplace_back(array[1]);
+    vec.emplace_back(array[2]);
+    vec.emplace_back(array[0]);
+    std::sort(std::begin(vec), std::end(vec));
+    REQUIRE(*(vec[0]) == 3);
+    REQUIRE(*(vec[1]) == 1);
+    REQUIRE(*(vec[2]) == 2);
+    REQUIRE(array[0] == 3);
+    REQUIRE(array[1] == 1);
+    REQUIRE(array[2] == 2);
+    std::sort(std::begin(vec), std::end(vec), [](auto const& lhs, auto const& rhs) {
+      return *lhs < *rhs;
+    });
+    REQUIRE(*(vec[0]) == 1);
+    REQUIRE(*(vec[1]) == 2);
+    REQUIRE(*(vec[2]) == 3);
+    REQUIRE(array[0] == 3);
+    REQUIRE(array[1] == 1);
+    REQUIRE(array[2] == 2);
+  }
+}
+}
