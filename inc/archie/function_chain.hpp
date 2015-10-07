eda103ffdb634e8bb5b5bab4abaf2001ce2a165d
Karol Wozniak <attu.git@email.com>
2015-10-07 19:40:18 +0200

    pure_function header

--- inc/archie/function_chain.hpp
+++ inc/archie/function_chain.hpp
@@ -1,7 +1,5 @@
 #pragma once
 #include <vector>
-#include <utility>
-#include <type_traits>
 
 namespace archie {
 template <typename...>
@@ -20,56 +18,5 @@ public:
     static_assert(std::is_empty<F>::value == true, "");
     base_t::push_back(f);
   }
-};
-
-template <typename...>
-struct pure_function;
-
-template <typename R, typename... Args>
-struct pure_function<R(Args...)> {
-  using type = R (*)(Args...);
-  using pointer = type;
-
-  pure_function() = default;
-
-  template <typename U>
-  explicit pure_function(
-      U u,
-      typename std::enable_if<std::is_convertible<U, pointer>::value, void*>::type = nullptr)
-      : fptr(u) {}
-
-  template <typename U>
-  explicit pure_function(
-      U,
-      typename std::enable_if<!std::is_convertible<U, pointer>::value && std::is_empty<U>::value,
-                              void*>::type = nullptr)
-      : pure_function([](Args... xs) { return (*static_cast<U*>(nullptr))(xs...); }) {}
-
-  template <typename... U>
-  auto operator()(U&&... u) const {
-    return (*fptr)(std::forward<U>(u)...);
-  }
-
-  template <typename U>
-  typename std::enable_if<std::is_convertible<U, pointer>::value, pure_function&>::type operator=(
-      U u) {
-    fptr = u;
-    return *this;
-  }
-
-  template <typename U>
-  typename std::enable_if<!std::is_convertible<U, pointer>::value && std::is_empty<U>::value,
-                          pure_function&>::type
-  operator=(U) {
-    fptr = [](Args... xs) { return (*static_cast<U*>(nullptr))(xs...); };
-    return *this;
-  }
-
-  explicit operator bool() const { return fptr != nullptr; }
-
-  operator pointer() const { return fptr; }
-
-private:
-  pointer fptr = nullptr;
 };
 }
