b7e3451f8b1bdeb87d91f3f2fbe202ac2394ac92
Karol Wozniak <attu.git@email.com>
2015-10-07 19:29:24 +0200

    pure_function

--- inc/archie/function_chain.hpp
+++ inc/archie/function_chain.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <utility>
+#include <type_traits>
 
 namespace archie {
 template <typename...>
@@ -18,5 +20,56 @@ public:
     static_assert(std::is_empty<F>::value == true, "");
     base_t::push_back(f);
   }
+};
+
+template <typename...>
+struct pure_function;
+
+template <typename R, typename... Args>
+struct pure_function<R(Args...)> {
+  using type = R (*)(Args...);
+  using pointer = type;
+
+  pure_function() = default;
+
+  template <typename U,
+            typename std::enable_if<std::is_convertible<U, pointer>::value, int>::type...>
+  explicit pure_function(U u)
+      : fptr(u) {}
+
+  template <
+      typename U,
+      typename std::enable_if<!std::is_convertible<U, pointer>::value && std::is_empty<U>::value,
+                              int>::type...>
+  explicit pure_function(U)
+      : pure_function([](Args... xs) { return (*static_cast<U*>(nullptr))(xs...); }) {}
+
+  template <typename... U>
+  auto operator()(U&&... u) const {
+    return (*fptr)(std::forward<U>(u)...);
+  }
+
+  template <typename U,
+            typename std::enable_if<std::is_convertible<U, pointer>::value, int>::type...>
+  pure_function& operator=(U u) {
+    fptr = u;
+    return *this;
+  }
+
+  template <
+      typename U,
+      typename std::enable_if<!std::is_convertible<U, pointer>::value && std::is_empty<U>::value,
+                              int>::type...>
+  pure_function& operator=(U) {
+    fptr = [](Args... xs) { return (*static_cast<U*>(nullptr))(xs...); };
+    return *this;
+  }
+
+  explicit operator bool() const { return fptr != nullptr; }
+
+  operator pointer() const { return fptr; }
+
+private:
+  pointer fptr = nullptr;
 };
 }
--- test/function_chain_test.cpp
+++ test/function_chain_test.cpp
@@ -26,4 +26,27 @@ TEST_CASE("Can use function chain", "[function_chain]") {
     REQUIRE(i == 15);
   }
 }
+
+int foo(int) { return 3; }
+struct bar {
+  int operator()(int) const { return 42; };
+};
+TEST_CASE("pure_function", "[pure_function]") {
+  pure_function<int(int)> p1(foo);
+  auto boo = [](int) { return 4; };
+  pure_function<int(int)> p2(boo);
+  bar b;
+  pure_function<int(int)> p3(b);
+  REQUIRE(p1(2) == foo(2));
+  REQUIRE(p2(2) == boo(2));
+  REQUIRE(p3(2) == b(2));
+  p1 = b;
+  p2 = foo;
+  p3 = boo;
+  REQUIRE(p1);
+  p1 = nullptr;
+  REQUIRE(!p1);
+  using ptr_t = int (*)(int);
+  ptr_t ptr = p2;
+}
 }
