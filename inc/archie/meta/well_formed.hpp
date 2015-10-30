a6404fd90c434b3098887da398cf693479b3d1f3
Karol Wozniak <attu.git@email.com>
2015-10-30 08:47:13 +0100

    hack for gcc < 5

--- inc/archie/meta/well_formed.hpp
+++ inc/archie/meta/well_formed.hpp
@@ -3,7 +3,16 @@
 
 namespace archie {
 namespace meta {
+#if __GNUC__ < 5
+  template <typename...>
+  struct well_formed_ {
+    using type = ignore_t;
+  };
+  template <typename... T>
+  using well_formed = typename well_formed_<T...>::type;
+#else
   template <typename...>
   using well_formed = ignore_t;
+#endif
 }
 }
