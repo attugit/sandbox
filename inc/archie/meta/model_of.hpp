d89d66c5ede4728e5dd2e8545f65e1ddf6d594e1
Karol Wozniak <attu.git@email.com>
2015-10-30 08:42:08 +0100

    gcc build: change fobj to constexpr func

--- inc/archie/meta/model_of.hpp
+++ inc/archie/meta/model_of.hpp
@@ -12,16 +12,9 @@ namespace meta {
                   well_formed<decltype(std::declval<Concept>().requires(std::declval<Args>()...))>>
       : std::true_type {};
 }
-namespace detail {
-  template <typename Concept>
-  struct model_of_ {
-    template <typename... Args>
-    constexpr decltype(auto) operator()(Args...) const noexcept {
-      return meta::model_of<Concept(Args...)>::value;
-    }
-  };
-}
 
-template <typename Concept>
-static constexpr auto const& model_of = meta::instance<detail::model_of_<Concept>>();
+template <typename Concept, typename... Args>
+constexpr bool model_of(Args...) {
+  return meta::model_of<Concept(Args...)>::value;
+}
 }
