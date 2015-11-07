09b8c3d8836f0801de7b9b88da3bcd5f4e53f0a6
Karol Wozniak <attu.git@email.com>
2015-11-07 13:37:26 +0100

    add constarints

--- inc/archie/container/ring_adapter.hpp
+++ inc/archie/container/ring_adapter.hpp
@@ -1,12 +1,26 @@
 #pragma once
 #include <utility>
 #include <archie/container/ring_iterator.hpp>
+#include <archie/meta/model_of.hpp>
 
 namespace archie {
 template <typename Container>
 struct ring_adapter {
 private:
   using container_t = Container;
+  struct is_valid_container {
+    template <typename C>
+    auto requires(C) -> decltype(std::declval<C const>().begin(),
+                                 std::declval<C const>().end(),
+                                 std::declval<C const>().size(),
+                                 std::declval<C const>().capacity());
+  };
+  struct can_emplace {
+    template <typename C, typename... Args>
+    auto requires(C, Args...) -> decltype(std::declval<C>().emplace_back(std::declval<Args>()...),
+                                          typename C::value_type{std::declval<Args>()...});
+  };
+  static_assert(meta::model_of<is_valid_container(Container)>::value == true, "");
 
 public:
   using value_type = typename container_t::value_type;
@@ -30,6 +44,7 @@ public:
 
   template <typename... Args>
   void emplace_back(Args&&... args) {
+    static_assert(meta::model_of<can_emplace(Container, Args...)>::value, "");
     if (size() != capacity()) {
       container_.emplace_back(std::forward<Args>(args)...);
       pos_ = iterator{container_, 0};
